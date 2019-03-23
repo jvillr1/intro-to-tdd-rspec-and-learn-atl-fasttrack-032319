d74bf90f03fb45689d566de8da191b52f96c2046
Jessica Villarreal <jvillr11@gmail.com>
2019-03-23 23:45:27 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,5 +1,3 @@
-require_relative '../current_age_for_birth_year.rb'
-
 RSpec.describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
     age_of_person = current_age_for_birth_year(select_year)
